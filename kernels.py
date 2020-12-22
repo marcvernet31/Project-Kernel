983373eb03d7003cb783b0c9ea15c942101a087b
jordipuig37 <jordipuig37@gmail.com>
2020-12-22 16:03:39 +0100

    kernels library

---
+++ kernels.py
@@ -0,0 +1,72 @@
+import numpy as np
+
+def overlap(X, Y):
+    xm, xn = X.shape
+    ym, yn = Y.shape
+    # Compute the kernel matrix:
+    G = np.zeros((xm, ym))
+    for i in range(xm):
+        Xi = np.tile(X[i], (ym, 1))
+        Xi = Xi == Y
+        G[i, :] = np.sum(Xi, axis=1)
+    return G
+
+def smc(X, Y):
+    xm, xn = X.shape
+    ym, yn = Y.shape
+    # Compute the kernel matrix:
+    G = np.zeros((xm, ym))
+    for i in range(xm):
+        Xi = np.tile(X[i], (ym, 1))
+        Xi = Xi == Y
+        G[i, :] = np.sum(Xi, axis=1) / xn
+    return G
+
+
+def jaccard(X, Y):
+    xm, xn = X.shape
+    ym, yn = Y.shape
+    # Compute the kernel matrix:
+    G = np.zeros((xm, ym))
+    for i in range(xm):
+        Xi = np.tile(X[i], (ym, 1))
+        Xi = Xi == Y
+        eq = np.sum(Xi, axis = 1)
+        G[i, :] = eq / (2*xn-eq)
+    return G # postf f(G)
+
+def jaccard2(X, Y):
+    xm, xn = X.shape
+    ym, yn = Y.shape
+    # Compute the kernel matrix:
+    G = np.zeros((xm, ym))
+    ytraits = np.sum(Y, axis=1)
+    for i in range(xm):
+        Xi = np.tile(X[i], (ym, 1))
+        xtraits = np.sum(Xi, axis = 1)
+        Xi = Xi == Y
+        eq = np.sum(Xi, axis = 1)
+        G[i, :] = eq / (xtraits+ytraits-eq)
+    return G
+
+def k0(X, Y): # what is this?
+    xm, xn = X.shape
+    ym, yn = Y.shape
+    # Compute the kernel matrix:
+    G = np.zeros((xm, ym))
+    for i in range(xm):
+        Xi = np.tile(X[i], (ym, 1))
+        Xi = Xi == Y
+        G[i, :] = np.mean(Xi, axis=1)
+    return G
+
+def k0prime(X, Y):
+    xm, xn = X.shape
+    ym, yn = Y.shape
+    G = np.zeros((xm, ym))
+    for i in range(xm):
+        Xi = np.tile(X[i], (ym, 1))
+        Xi = Xi == Y
+        G[i, :] = np.mean(Xi, axis=1)
+    gamma = 1
+    return np.exp(gamma * G)
